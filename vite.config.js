11febc4a1bcd25ec13eebec41debc241e2e8029a
Alexandr Maspanov <alexandrmaspanov@gmail.com>
2025-07-30 13:05:29 +0300

    chore: set up deployment with gh-pages and updated package.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
+  base: '/vitals-monitor-prototype/',
   plugins: [
     react(),
     svgr({
